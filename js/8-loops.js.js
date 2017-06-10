d8f72c95cfa607bcc88c529a24b3ad92853177a3
Maciej Abramowski <m.abramowski@wp.pl>
2017-06-10 13:39:46 +0200

    do while

--- js/8-loops.js.js
+++ js/8-loops.js.js
@@ -8,4 +8,17 @@ for ( var i=4; i>=0; i--){
     console.log(i);
 }
 
+var it = 0;
+while (it<10){
+       console.log("Pętla while [it = " + it + "]");
+    
+        it++;
+       }
+
+var iter = 20;
+do {
+    console.log(iter);
+    iter++;
+} while (iter < 10)
+
 
